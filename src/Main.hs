c239b31d370511fd98f2e480572d4b0ec1eeb6c5
Rene Klacan <rene.klacan@gmail.com>
2015-06-02 23:43:39 +0100

    Fix all warnings

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,6 @@
 import System.Environment (getArgs)
 import Data.List
 import Data.List.Split (splitOn)
-import System.Process
 import System.Exit
 
 ------------------------------------------------------------------------------
@@ -22,7 +21,7 @@ instance Ord Path where
     (Path _ _ f1) `compare` (Path _ _ f2) = f2 `compare` f1
 
 instance Ord ScoredPath where
-    (ScoredPath p1 s1) `compare` (ScoredPath p2 s2) = (s2, p2) `compare` (s1, p2)
+    (ScoredPath p1 s1) `compare` (ScoredPath p2 s2) = (s2, p2) `compare` (s1, p1)
 
 ------------------------------------------------------------------------------
 
@@ -46,7 +45,7 @@ cdPath Nothing = do
     exitFailure
 
 loadAllPaths :: [String] -> [Path]
-loadAllPaths lines = map loadPath $ frequency lines
+loadAllPaths ls = map loadPath $ frequency ls
 
 frequency :: Eq a => [a] -> [(a, Int)]
 frequency l = map (\x -> (head x, length x)) $ group l
@@ -75,6 +74,7 @@ getPathScore [] _ = 10
 getPathScore _ [] = 0
 
 getBestPath :: [ScoredPath] -> Maybe Path
-getBestPath (sp:x)
+getBestPath [] = Nothing
+getBestPath (sp:_)
     | spScore sp > 0 = Just $ spPath sp
     | otherwise      = Nothing
